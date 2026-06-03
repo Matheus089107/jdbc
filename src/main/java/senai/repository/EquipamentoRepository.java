b544974ee5b5e113ec3a9d7121ef6d4737bcf31d
MATHEUS ALEXANDRE DE CASTRO <matheus_a_castro@estudante.sesisenai.org.br>
2026-06-03 14:17:58 -0300

    feat: termina o resto do CRUD do equipamento

--- src/main/java/senai/repository/EquipamentoRepository.java
+++ src/main/java/senai/repository/EquipamentoRepository.java
@@ -7,42 +7,107 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EquipamentoRepository {
 
-    public Equipamento cadastrar(Equipamento equipamento) throws SQLException{
-      String query = """
-              INSERT INTO Equipamento (
-                            nome,
-                            numero_serie,
-                            fornecedor_id
-                            ) VALUES
-                            (?,?,?);
-              """;
+    public Equipamento cadastrar(Equipamento equipamento) throws SQLException {
+        String query = """
+                INSERT INTO Equipamento (
+                              nome,
+                              numero_serie,
+                              fornecedor_id
+                              ) VALUES
+                              (?,?,?);
+                """;
 
-      try(Connection conn = Conexao.conectar();
-          PreparedStatement stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)){
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-          stmt.setString(1, equipamento.getNome());
-          stmt.setString(2, equipamento.getNumeroSerie());
-          stmt.setInt(3, equipamento.getFornecedorId());
-          stmt.executeUpdate();
+            stmt.setString(1, equipamento.getNome());
+            stmt.setString(2, equipamento.getNumeroSerie());
+            stmt.setInt(3, equipamento.getFornecedorId());
+            stmt.executeUpdate();
 
-          ResultSet rs = stmt.getGeneratedKeys();
+            ResultSet rs = stmt.getGeneratedKeys();
 
-          if(rs.next()){
-              equipamento.setId(rs.getInt(1));
-          }
+            if (rs.next()) {
+                equipamento.setId(rs.getInt(1));
+            }
 
-      }
-      return equipamento;
+        }
+        return equipamento;
     }
 
-    public Equipamento buscarPorId(int id) throws SQLException{
+    public Equipamento buscarPorId(int id) throws SQLException {
         String query = """
-                
+                SELECT id, nome, numero_serie, fornecedor_id FROM Equipamento where id = ?;
                 """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new Equipamento(rs.getInt("id"), rs.getString("nome"), rs.getString("numero_serie"), rs.getInt("fornecedor_id"));
+            }
+        }
         return null;
+    }
+
+    public List<Equipamento> buscaPorFornecedorId(int fornecedorId) throws SQLException {
+        String query = """
+                SELECT e.id, e.nome, e.numero_serie, e.fornecedor_id FROM Equipamento e
+                JOIN Fornecedor f
+                ON f.id = e.fornecedor_id
+                WHERE e.fornecedor_id = ?;
+                """;
+
+       List<Equipamento> listEquipamento = new ArrayList<>();
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, fornecedorId);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                listEquipamento.add(
+                new Equipamento(rs.getInt("id"), rs.getString("nome"), rs.getString("numero_serie"), rs.getInt("fornecedor_id")));
+            }
+        }
+        return listEquipamento;
+    }
+
+    public Equipamento updEquipamento(Equipamento equipamento) throws SQLException{
+        String query = """
+                UPDATE Equipamento SET nome = ?, numero_serie = ? WHERE id = ?
+                """;
+
+        try(Connection conn = Conexao.conectar();
+        PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setString(1, equipamento.getNome());
+            stmt.setString(2, equipamento.getNumeroSerie());
+            stmt.setInt(3, equipamento.getId());
+            stmt.executeUpdate();
+        }
+        return equipamento;
+    }
+
+    public void dltEquipamento(int id) throws SQLException{
+        String query = """
+                DELETE FROM Equipamento WHERE id = ?;
+                """;
+        try(Connection conn = Conexao.conectar();
+        PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
     }
 
 }
--- src/main/java/senai/service/equipamento/EquipamentoServiceImpl.java
+++ src/main/java/senai/service/equipamento/EquipamentoServiceImpl.java
@@ -1,7 +1,9 @@
 package senai.service.equipamento;
 
 import senai.model.Equipamento;
+import senai.model.Fornecedor;
 import senai.repository.EquipamentoRepository;
+import senai.repository.FornecedorRepository;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -9,28 +11,50 @@ import java.util.List;
 public class EquipamentoServiceImpl implements EquipamentoService{
 
     EquipamentoRepository repository = new EquipamentoRepository();
+    FornecedorRepository fornecedorRepository=new FornecedorRepository();
+
+
     @Override
     public Equipamento criarEquipamento(Equipamento equipamento) throws SQLException {
+        Fornecedor fornecedor = fornecedorRepository.buscarPorId(equipamento.getFornecedorId());
+        if(fornecedor == null){
+            throw new RuntimeException("Fornecedor inválido ou inexistente!");
+        }
         return repository.cadastrar(equipamento);
     }
 
     @Override
     public Equipamento buscarPorId(int id) throws SQLException {
-        return null;
+        Equipamento equipamento = repository.buscarPorId(id);
+
+        if(equipamento == null){
+            throw new RuntimeException("Id do Equipamento não encontrado!");
+        }
+        return equipamento;
     }
 
     @Override
     public List<Equipamento> buscarPorFornecedorId(int fornecedorId) throws SQLException {
-        return List.of();
+
+        return repository.buscaPorFornecedorId(fornecedorId);
     }
 
     @Override
     public void atualizarEquipamento(Equipamento equipamento) throws SQLException {
 
+        Fornecedor fornecedor = fornecedorRepository.buscarPorId(equipamento.getFornecedorId());
+        if(fornecedor == null){
+            throw new RuntimeException("Equipamento não encontrado para atualização!");
+        }
+        repository.updEquipamento(equipamento);
     }
 
     @Override
     public void deletarEquipamento(int id) throws SQLException {
-
+            Equipamento equipamento = repository.buscarPorId(id);
+            if(equipamento == null){
+                throw new RuntimeException("Equipamento não encontrado para exclusão!");
+            }
+            repository.dltEquipamento(id);
     }
 }
